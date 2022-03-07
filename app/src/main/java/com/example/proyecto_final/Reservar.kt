47749eab9cfda79f7111803a4087507e652ed59a
emilyg0310 <egonzalez70453@ufide.ac.cr>
2022-03-06 23:30:44 -0600

    Diseño Reservar Visita

--- app/src/main/java/com/example/proyecto_final/Reservar.kt
+++ app/src/main/java/com/example/proyecto_final/Reservar.kt
@@ -1,11 +1,26 @@
 package com.example.proyecto_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ImageButton
+import android.widget.Spinner
 
 class Reservar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reservar)
+
+        val atras = findViewById<ImageButton>(R.id.atras)
+        atras.setOnClickListener{
+            val pase_vista = Intent(this, Inicio::class.java)
+            startActivity(pase_vista)
+        }
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+
+
     }
+
 }
