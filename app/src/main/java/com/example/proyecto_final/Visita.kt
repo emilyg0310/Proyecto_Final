6b6a9ef44005b192c1c8ce45b995e0444cf39dac
emilyg0310 <egonzalez70453@ufide.ac.cr>
2022-03-10 17:42:56 -0600

    Diseño Perfil/Reserva

--- app/src/main/java/com/example/proyecto_final/Visita.kt
+++ app/src/main/java/com/example/proyecto_final/Visita.kt
@@ -1,11 +1,20 @@
 package com.example.proyecto_final
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Visita : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visita)
+
+
+        val atras = findViewById<ImageButton>(R.id.atras)
+        atras.setOnClickListener{
+            val pase_vista = Intent(this, Inicio::class.java)
+            startActivity(pase_vista)
+        }
     }
 }
